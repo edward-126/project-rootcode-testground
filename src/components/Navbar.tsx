1845b55da142a135eb230c207cf4fd6911aade60
Edward-126 <edwardhyde20126@gmail.com>
2024-08-30 13:43:28 +0530

    feat: navbar added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React from "react";
+import MaxWidthWrapper from "./shared/MaxWidthWrapper";
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="sticky top-0 z-50 h-20 border-b border-border transition-all duration-200">
+        <MaxWidthWrapper className="flex h-full items-center justify-between">
+          <Link href="/">
+            <div className="text-lg font-semibold">Project-Test-Rootcode</div>
+          </Link>
+          <Button asChild variant="outline">
+            <Link href="/addTopic">Add Topic</Link>
+          </Button>
+        </MaxWidthWrapper>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
